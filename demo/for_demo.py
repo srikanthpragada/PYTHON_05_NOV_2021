de9e57748f3008b377f0a03d640cd08f6c99cc9d
srikanthpragada <srikanthpragada@gmail.com>
2021-11-10 07:55:00 +0530

    10-NOV

---
+++ demo/for_demo.py
@@ -0,0 +1,9 @@
+# print number 1 to 10
+
+for n in range(1, 11):
+    print(n, end=' ')
+
+print()
+
+for n in range(10, 0, -1):
+    print(n, end=' ')
---
+++ demo/while_demo.py
@@ -0,0 +1,15 @@
+# print 1 to 10
+
+n = 1
+while n <= 10:
+    print(n, end=' ')
+    n = n + 1
+
+print()   # print new line
+
+n = 10
+while n >= 1:
+    print(n, end=' ')
+    n = n - 1
+
+print("\nThe End")
