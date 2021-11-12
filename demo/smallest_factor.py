27eed5194f8d843e7ae2f890994037e04e013b8f
srikanthpragada <srikanthpragada@gmail.com>
2021-11-12 07:56:17 +0530

    12-NOV

---
+++ demo/smallest_factor.py
@@ -0,0 +1,13 @@
+# Print smallest factor for the given number
+
+num = int(input("Enter number :"))
+
+for i in range(2, num//2 + 1):
+    if num % i == 0:
+        print(i)
+        break
+else:
+    print(num)
+
+
+
---
+++ demo/sum_of_10_numbers.py
@@ -0,0 +1,11 @@
+
+total = 0
+for i in range(10):
+    num = int(input("Enter number [0 to stop]: "))
+    if num == 0:
+        break
+
+    total += num
+
+
+print(f"Total = {total}")
