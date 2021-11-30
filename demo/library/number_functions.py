2ca8db3cd763175da5f5c9f0ed13553a408dd107
srikanthpragada <srikanthpragada@gmail.com>
2021-11-30 07:59:07 +0530

    30-NOV

---
+++ demo/library/number_functions.py
@@ -0,0 +1,18 @@
+def iseven(n):
+    """
+    Returns true if number is even otherwise false
+    n : number to test
+    """
+    return n % 2 == 0
+
+
+def isprime(n):
+    pass
+
+
+def ispalindrome(n):
+    pass
+
+
+if __name__ == '__main__':     # run as script
+    print("Number Functions Ver 1.0")
---
+++ demo/library/test.py
@@ -0,0 +1,5 @@
+
+import number_functions as nf
+from number_functions import isprime
+
+print(nf.iseven(10), isprime(11))
