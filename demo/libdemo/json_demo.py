bdecb7181a81286ac3c61d7805775e8a4e8a5667
srikanthpragada <srikanthpragada@gmail.com>
2021-12-14 07:56:42 +0530

    14-DEC

---
+++ demo/libdemo/json_demo.py
@@ -0,0 +1,14 @@
+import json
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} - {self.age}"
+
+
+p = Person("A", 20)
+print(json.dumps(p.__dict__))
